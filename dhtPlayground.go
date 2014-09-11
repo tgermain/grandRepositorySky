45190ac019e8ecbf98d4144a7a9de665859c8804
tgermain <timothee.germain@wanadoo.fr>
2014-09-11 23:34:50 +0200

    changing id type to byte[]

--- dhtPlayground.go
+++ dhtPlayground.go
@@ -1,41 +1,46 @@
-package dht
+package grandRepositorySky
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/tgermain/grandRepositorySky/dht"
 )
 
 type DHTnode struct {
-	id       int
+	id       []byte
 	ring     []*DHTnode
 	ip, port string
 }
 
-func makeDHTNode(id int) DHTnode {
+func MakeDHTNode(id string) DHTnode {
 	daNode := DHTnode{
-		id:   id,
+		id:   []byte(id),
 		ring: make([]*DHTnode, 1),
 	}
 	daNode.ring[0] = &daNode
 	return daNode
 }
 
-func (currentNode *DHTnode) addToRing(newNode DHTnode) {
+func (currentNode *DHTnode) AddToRing(newNode DHTnode) {
 	//furthers comment assume that he current currentNode is named x
 	switch {
 
-	case (currentNode.id == currentNode.ring[0].id):
-		{
-			//init case : currentNode looping on itself
-			newNode.ring[0] = currentNode.ring[0]
-			currentNode.ring[0] = &newNode
-		}
-	case (currentNode.id < newNode.id) && (newNode.id < currentNode.ring[0].id):
+	// case (currentNode.id == currentNode.ring[0].id):
+	// 	{
+	// 		//init case : currentNode looping on itself
+	// 		newNode.ring[0] = currentNode.ring[0]
+	// 		currentNode.ring[0] = &newNode
+	// 	}
+
+	case dht.Between(currentNode.id, currentNode.ring[0].id, newNode.id):
+		// (currentNode.id < newNode.id) && (newNode.id < currentNode.ring[0].id)
 		{
 			//case of x->(x+2) and we want to add (x+1) node
 			newNode.ring[0] = currentNode.ring[0]
 			currentNode.ring[0] = &newNode
 		}
-	case (currentNode.id < newNode.id) && (currentNode.ring[0].id < currentNode.id):
+	case dht.Between(currentNode.ring[0].id, newNode.id, currentNode.id):
+		// (currentNode.ring[0].id < currentNode.id) && (currentNode.id < newNode.id)
 		{
 			//case of X -> 0 and we want to add (x+1) node
 			newNode.ring[0] = currentNode.ring[0]
@@ -43,21 +48,19 @@ func (currentNode *DHTnode) addToRing(newNode DHTnode) {
 		}
 	default:
 		{
-			currentNode.ring[0].addToRing(newNode)
+			currentNode.ring[0].AddToRing(newNode)
 		}
 	}
 }
 
-func (node *DHTnode) printRing() {
-	fmt.Printf("%v\n", node.id)
-	if node.ring[0] != nil {
-		node.ring[0].printRingRec(node.id)
-	}
+func (node *DHTnode) PrintRing() {
+	fmt.Printf("%s\n", node.id)
+	node.ring[0].printRingRec(node.id)
 }
 
-func (node *DHTnode) printRingRec(origId int) {
-	fmt.Printf("%v\n", node.id)
-	if node.ring[0].id != origId {
+func (node *DHTnode) printRingRec(origId []byte) {
+	fmt.Printf("%s\n", node.id)
+	if bytes.Compare(node.ring[0].id, origId) != 0 {
 
 		node.ring[0].printRingRec(origId)
 	}
--- dhtPlayground_test.go
+++ dhtPlayground_test.go
@@ -1,4 +1,4 @@
-package dht
+package grandRepositorySky
 
 import (
 	"fmt"
@@ -7,27 +7,27 @@ import (
 )
 
 func TestPlaygGroundRingSetup(t *testing.T) {
-	node0 := makeDHTNode(00)
-	node1 := makeDHTNode(01)
-	node2 := makeDHTNode(02)
-	node3 := makeDHTNode(03)
-	node4 := makeDHTNode(04)
-	node5 := makeDHTNode(05)
-	node6 := makeDHTNode(06)
-	node7 := makeDHTNode(07)
+	node0 := MakeDHTNode("00")
+	node1 := MakeDHTNode("01")
+	node2 := MakeDHTNode("02")
+	node3 := MakeDHTNode("03")
+	node4 := MakeDHTNode("04")
+	node5 := MakeDHTNode("05")
+	node6 := MakeDHTNode("06")
+	node7 := MakeDHTNode("07")
 
-	node0.addToRing(node1)
-	node1.addToRing(node2)
-	node1.addToRing(node3)
-	node1.addToRing(node4)
-	node4.addToRing(node5)
-	node3.addToRing(node6)
-	node3.addToRing(node7)
+	node0.AddToRing(node1)
+	node1.AddToRing(node2)
+	node1.AddToRing(node3)
+	node1.AddToRing(node4)
+	node4.AddToRing(node5)
+	node3.AddToRing(node6)
+	node3.AddToRing(node7)
 
 	fmt.Println("------------------------------------------------------------------------------------------------")
 	fmt.Println("RING STRUCTURE")
 	fmt.Println("------------------------------------------------------------------------------------------------")
-	node1.printRing()
+	node1.PrintRing()
 	fmt.Println("------------------------------------------------------------------------------------------------")
 	assert.Equal(t, node4.ring[0].id, node5.id, "05 should follow 04")
 	assert.Equal(t, node7.ring[0].id, node0.id, "last node should go to the beginning")
