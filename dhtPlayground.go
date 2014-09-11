e50d2ab1b622648929e16d8d1657041c3b43c951
tgermain <timothee.germain@wanadoo.fr>
2014-09-11 22:41:05 +0200

    removing unneeded print

--- dhtPlayground.go
+++ dhtPlayground.go
@@ -20,34 +20,27 @@ func makeDHTNode(id int) DHTnode {
 
 func (currentNode *DHTnode) addToRing(newNode DHTnode) {
 	//furthers comment assume that he current currentNode is named x
-	fmt.Printf("adding %v       ", newNode.id)
 	switch {
 
 	case (currentNode.id == currentNode.ring[0].id):
 		{
-			//init case 1 currentNode looping on itself
-			fmt.Println("Init adding second currentNode")
+			//init case : currentNode looping on itself
 			newNode.ring[0] = currentNode
 			currentNode.ring[0] = &newNode
 		}
 	case (currentNode.id < newNode.id) && (currentNode.ring[0].id > newNode.id):
 		{
-
-			//case of x -> x+2 and we want to add x+1 currentNode
-			fmt.Println("C'est bon on ajoute")
-			fmt.Printf("After %q come %q \n", currentNode.id, newNode.id)
+			//case of x->(x+2) and we want to add (x+1) node
 			currentNode.ring[0] = &newNode
 		}
 	case (currentNode.id < newNode.id) && (currentNode.ring[0].id < currentNode.id):
 		{
-			//case of X -> 0 and we want to add x+1
-			fmt.Println("Adding at the end of the ring")
+			//case of X -> 0 and we want to add (x+1) node
 			newNode.ring[0] = currentNode.ring[0]
 			currentNode.ring[0] = &newNode
 		}
 	default:
 		{
-			fmt.Println("on passe au suivant")
 			currentNode.ring[0].addToRing(newNode)
 		}
 	}
