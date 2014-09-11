f6ad108b9bf6a6558924833bc77fd484fe90a119
tgermain <timothee.germain@wanadoo.fr>
2014-09-11 19:46:42 +0200

    working addition of node : 	they are added correctly in the good order

--- dhtPlayground.go
+++ dhtPlayground.go
@@ -2,23 +2,68 @@ package dht
 
 import (
 	"fmt"
-	"math"
 )
 
 type DHTnode struct {
-	id string
-	// ring := DHTnode[]
+	id   int
+	ring []*DHTnode
 }
 
-func makeDHTNode(id string) DHTnode {
-	return DHTnode{id: id}
+func makeDHTNode(id int) DHTnode {
+	daNode := DHTnode{
+		id:   id,
+		ring: make([]*DHTnode, 1),
+	}
+	daNode.ring[0] = &daNode
+	return daNode
 }
 
-func (node *DHTnode) addToRing(newNode DHTnode) {
-	fmt.Println("Here come a new node in the ring : ", newNode.id)
+func (currentNode *DHTnode) addToRing(newNode DHTnode) {
+	//furthers comment assume that he current currentNode is named x
+	fmt.Printf("adding %v       ", newNode.id)
+	switch {
+
+	case (currentNode.id == currentNode.ring[0].id):
+		{
+			//init case 1 currentNode looping on itself
+			fmt.Println("Init adding second currentNode")
+			newNode.ring[0] = currentNode
+			currentNode.ring[0] = &newNode
+		}
+	case (currentNode.id < newNode.id) && (currentNode.ring[0].id > newNode.id):
+		{
+
+			//case of x -> x+2 and we want to add x+1 currentNode
+			fmt.Println("C'est bon on ajoute")
+			fmt.Printf("After %q come %q \n", currentNode.id, newNode.id)
+			currentNode.ring[0] = &newNode
+		}
+	case (currentNode.id < newNode.id) && (currentNode.ring[0].id < currentNode.id):
+		{
+			//case of X -> 0 and we want to add x+1
+			fmt.Println("Adding at the end of the ring")
+			newNode.ring[0] = currentNode.ring[0]
+			currentNode.ring[0] = &newNode
+		}
+	default:
+		{
+			fmt.Println("on passe au suivant")
+			currentNode.ring[0].addToRing(newNode)
+		}
+	}
 }
 
-func main() {
-	fmt.Printf("hello, world\n")
-	fmt.Println(math.Pi)
+func (node *DHTnode) printRing() {
+	fmt.Printf("%v\n", node.id)
+	if node.ring[0] != nil {
+		node.ring[0].printRingRec(node.id)
+	}
+}
+
+func (node *DHTnode) printRingRec(origId int) {
+	fmt.Printf("%v\n", node.id)
+	if node.ring[0].id != origId {
+
+		node.ring[0].printRingRec(origId)
+	}
 }
