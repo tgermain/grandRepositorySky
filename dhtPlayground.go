790749c4cf691738ea4386e01395bc6167332315
tgermain <timothee.germain@wanadoo.fr>
2014-09-12 14:13:01 +0200

    -changing the type of id from []byte to string -add a working naive lookup method

--- dhtPlayground.go
+++ dhtPlayground.go
@@ -7,14 +7,14 @@ import (
 )
 
 type DHTnode struct {
-	id       []byte
+	id       string
 	ring     []*DHTnode
 	ip, port string
 }
 
 func MakeDHTNode(id string) DHTnode {
 	daNode := DHTnode{
-		id:   []byte(id),
+		id:   id,
 		ring: make([]*DHTnode, 1),
 	}
 	daNode.ring[0] = &daNode
@@ -22,20 +22,20 @@ func MakeDHTNode(id string) DHTnode {
 }
 
 func (currentNode *DHTnode) AddToRing(newNode DHTnode) {
-	//furthers comment assume that he current currentNode is named x
+	//furthers comments assume that he current currentNode is named x
 	switch {
-
-	// case (currentNode.id == currentNode.ring[0].id):
-	// 	{
-	// 		//init case : currentNode looping on itself
-	// 		newNode.ring[0] = currentNode.ring[0]
-	// 		currentNode.ring[0] = &newNode
-	// 	}
-
+	case bytes.Compare([]byte(currentNode.id), []byte(currentNode.ring[0].id)) == 0:
+		{
+			//init case : currentNode looping on itself
+			// fmt.Println("Init case : 2 node")
+			newNode.ring[0] = currentNode.ring[0]
+			currentNode.ring[0] = &newNode
+		}
 	case dht.Between(currentNode.id, currentNode.ring[0].id, newNode.id):
 		// (currentNode.id < newNode.id) && (newNode.id < currentNode.ring[0].id)
 		{
 			//case of x->(x+2) and we want to add (x+1) node
+			// fmt.Println("add in the middle")
 			newNode.ring[0] = currentNode.ring[0]
 			currentNode.ring[0] = &newNode
 		}
@@ -43,13 +43,23 @@ func (currentNode *DHTnode) AddToRing(newNode DHTnode) {
 		// (currentNode.ring[0].id < currentNode.id) && (currentNode.id < newNode.id)
 		{
 			//case of X -> 0 and we want to add (x+1) node
+			// fmt.Println("add at the end")
 			newNode.ring[0] = currentNode.ring[0]
 			currentNode.ring[0] = &newNode
 		}
 	default:
 		{
+			// fmt.Println("Go to the next")
 			currentNode.ring[0].AddToRing(newNode)
 		}
+	}
+}
+
+func (currentNode *DHTnode) Lookup(idToSearch string) string {
+	if dht.Between(currentNode.id, currentNode.ring[0].id, idToSearch) {
+		return currentNode.id
+	} else {
+		return currentNode.ring[0].Lookup(idToSearch)
 	}
 }
 
@@ -58,9 +68,9 @@ func (node *DHTnode) PrintRing() {
 	node.ring[0].printRingRec(node.id)
 }
 
-func (node *DHTnode) printRingRec(origId []byte) {
+func (node *DHTnode) printRingRec(origId string) {
 	fmt.Printf("%s\n", node.id)
-	if bytes.Compare(node.ring[0].id, origId) != 0 {
+	if bytes.Compare([]byte(node.ring[0].id), []byte(origId)) != 0 {
 
 		node.ring[0].printRingRec(origId)
 	}
