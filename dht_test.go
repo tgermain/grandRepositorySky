01194fe6e52efd794bd2010f27c04b2adfd0512a
tgermain <timothee.germain@wanadoo.fr>
2014-10-02 13:00:00 +0200

    enhance the basic test (reale assert now)

--- dht_test.go
+++ dht_test.go
@@ -2,6 +2,7 @@ package grandRepositorySky
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"github.com/tgermain/grandRepositorySky/dht"
 	"testing"
 )
@@ -22,32 +23,61 @@ import (
  * d8b6ac320d92fe71551bed2f702ba6ef2907283e 1237215742469423719453176640534983456657032816702
  * ee33f5aaf7cf6a7168a0f3a4449c19c9b4d1e399 1359898542148650805696846077009990511357036979097
  */
-func TestRingSetup(t *testing.T) {
+func TestRingSetup160bit(t *testing.T) {
+	id1 := "01"
+	id2 := "02"
+	id3 := "03"
+	id4 := "04"
+	id5 := "05"
+	id6 := "06"
+	id7 := "07"
+	id8 := "08"
+	id9 := "09"
 	// note nil arg means automatically generate ID, e.g. f38f3b2dcc69a2093f258e31902e40ad33148385
-	node1 := MakeDHTNode(nil, "localhost", "1111")
-	node2 := MakeDHTNode(nil, "localhost", "1112")
-	// node3 := MakeDHTNode(nil, "localhost", "1113")
-	// node4 := MakeDHTNode(nil, "localhost", "1114")
-	// node5 := MakeDHTNode(nil, "localhost", "1115")
-	// node6 := MakeDHTNode(nil, "localhost", "1116")
-	// node7 := MakeDHTNode(nil, "localhost", "1117")
-	// node8 := MakeDHTNode(nil, "localhost", "1118")
-	// node9 := MakeDHTNode(nil, "localhost", "1119")
+	node1 := MakeDHTNode(&id1, "localhost", "1111")
+	node2 := MakeDHTNode(&id2, "localhost", "1112")
+	node3 := MakeDHTNode(&id3, "localhost", "1113")
+	node4 := MakeDHTNode(&id4, "localhost", "1114")
+	node5 := MakeDHTNode(&id5, "localhost", "1115")
+	node6 := MakeDHTNode(&id6, "localhost", "1116")
+	node7 := MakeDHTNode(&id7, "localhost", "1117")
+	node8 := MakeDHTNode(&id8, "localhost", "1118")
+	node9 := MakeDHTNode(&id9, "localhost", "1119")
 
 	node1.AddToRing(node2)
-	// node1.AddToRing(node3)
-	// node1.AddToRing(node4)
-	// node4.AddToRing(node5)
-	// node3.AddToRing(node6)
-	// node3.AddToRing(node7)
-	// node3.AddToRing(node8)
-	// node7.AddToRing(node9)
+	node1.AddToRing(node3)
+	node1.AddToRing(node4)
+	node4.AddToRing(node5)
+	node3.AddToRing(node6)
+	node3.AddToRing(node7)
+	node3.AddToRing(node8)
+	node7.AddToRing(node9)
+
+	var checking = []struct {
+		in         *DHTnode
+		next, prev string
+	}{
+		{node1, "02", "09"},
+		{node2, "03", "01"},
+		{node3, "04", "02"},
+		{node4, "05", "03"},
+		{node5, "06", "04"},
+		{node6, "07", "05"},
+		{node7, "08", "06"},
+		{node8, "09", "07"},
+		{node9, "01", "08"},
+	}
 
 	fmt.Println("------------------------------------------------------------------------------------------------")
 	fmt.Println("RING STRUCTURE")
 	fmt.Println("------------------------------------------------------------------------------------------------")
 	node1.PrintRing()
 	fmt.Println("------------------------------------------------------------------------------------------------")
+	for _, v := range checking {
+
+		assert.Equal(t, v.in.successor.tmp.id, v.next, "Error in successor")
+		assert.Equal(t, v.in.predecessor.tmp.id, v.prev, "Error in predecessor")
+	}
 }
 
 /*
