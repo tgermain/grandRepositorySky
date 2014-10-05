ba8ceb73550d1974ccad7451c99f30ec85d80354
tgermain <timothee.germain@wanadoo.fr>
2014-10-05 18:11:06 +0200

    working and tested method to send data to a distantNode

--- comLink/comLink.go
+++ comLink/comLink.go
@@ -3,25 +3,28 @@ package comLink
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 )
 
-type nodeDescriptor struct {
-	Ip   string
-	Port string
-}
-
+//Objects parts ---------------------------------------------------------
 type message struct {
 	TypeOfMsg   string
 	Id          string
-	Origin      nodeDescriptor
-	Destination nodeDescriptor
+	Origin      DistantNode
+	Destination DistantNode
 	Parameters  map[string][]byte
 }
 
+//TODO use grandRepositorySky.DistantNode when comLink is done
+type DistantNode struct {
+	Id, Ip, Port string
+}
+
+//Method parts ----------------------------------------------------------
 func marshallMessage(msg *message) []byte {
 	buffer, err := json.Marshal(msg)
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println("error while marshalling:", err)
 	}
 	// fmt.Printf("Marshalized message :%s\n", buffer)
 
@@ -32,9 +35,30 @@ func unmarshallMessage(buffer []byte) message {
 	var msg message
 	err := json.Unmarshal(buffer, &msg)
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println("error while unmarshalling:", err)
 	}
 	// fmt.Printf("Unmarshalized message :%+v\n", msg)
 
 	return msg
+}
+
+func settingUpUdpConnection(destination DistantNode) *net.UDPConn {
+	//Make sur you close the conection with defer conn.Close()
+	serverAddr, err := net.ResolveUDPAddr("udp", destination.Ip+":"+destination.Port)
+	if err != nil {
+		fmt.Println("error when resolving udp address:", err)
+	}
+	conn, err := net.DialUDP("udp", nil, serverAddr)
+	if err != nil {
+		fmt.Println("error when connecting to udp:", err)
+	}
+	return conn
+}
+
+func sendTo(destination DistantNode, payload []byte) {
+	conn := settingUpUdpConnection(destination)
+	defer conn.Close()
+
+	conn.Write(payload)
+	fmt.Printf("sending %s\n", payload)
 }
--- comLink/comLink_test.go
+++ comLink/comLink_test.go
@@ -3,6 +3,7 @@ package comLink
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"net"
 	"reflect"
 	"testing"
 )
@@ -11,21 +12,66 @@ func TestMarshallingUnmarshalling(t *testing.T) {
 	aMessage := message{
 		TypeOfMsg: "lookup",
 		Id:        "monIdQuIlEstBien",
-		Origin: nodeDescriptor{
+		Origin: DistantNode{
+			"IDOrigine",
 			"IPOrigine",
 			"PortOrigine",
 		},
-		Destination: nodeDescriptor{
+		Destination: DistantNode{
+			"IDDestination",
 			"IPDestination",
 			"PortDestination",
 		},
 		Parameters: make(map[string][]byte),
 	}
-	fmt.Printf("%+v\n", aMessage)
+
 	marshalledMsg := marshallMessage(&aMessage)
 
 	transformedMsg := unmarshallMessage(marshalledMsg)
-	fmt.Printf("%+v\n", transformedMsg)
-	assert.True(t, reflect.DeepEqual(aMessage, transformedMsg), "marshalling-unmarshalling should not change the message")
 
+	assert.True(t, reflect.DeepEqual(aMessage, transformedMsg), "marshalling-unmarshalling should not change the message")
+}
+
+func TestEffectifSendingReceving(t *testing.T) {
+
+	aMessage := message{
+		TypeOfMsg: "lookup",
+		Id:        "monIdQuIlEstBien",
+		Origin: DistantNode{
+			"IDOrigine",
+			"IPOrigine",
+			"PortOrigine",
+		},
+		Destination: DistantNode{
+			"IDDestination",
+			"",
+			"2000",
+		},
+		Parameters: make(map[string][]byte),
+	}
+
+	marshalledMsg := marshallMessage(&aMessage)
+
+	//seting up a test serveur ready t listen
+	addr, err := net.ResolveUDPAddr("udp", ":2000")
+	if err != nil {
+		fmt.Println("error when connecting to udp:", err)
+	}
+	sock, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println("error when listenning to udp:", err)
+	}
+	go func(c net.Conn) {
+		buf := make([]byte, 1024)
+		n, err := c.Read(buf)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+
+		assert.True(t, reflect.DeepEqual(marshalledMsg, buf[0:n]), "server should read what we send")
+		defer sock.Close()
+	}(sock)
+
+	sendTo(aMessage.Destination, marshalledMsg)
+	fmt.Println("Message sended")
 }
