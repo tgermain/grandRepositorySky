ebab74a5bea876595a7ff4bf77d784766570c084
Aline Martin <aline.martin@insa-lyon.fr>
2014-10-07 00:53:27 +0200

    working first example of server (searching node infos)
    brute text answer
    
    first routing :
    / -> hello world
    /nodes -> node1.toString
    /node/{idNode} 	-> if idNode == "01" node1.toString
    		-> else err message

--- web/server.go
+++ web/server.go
@@ -3,14 +3,42 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	//"github.com/tgermain/grandRepositorySky"
+	"github.com/tgermain/grandRepositorySky/dht" // for makeDHTnode
+	"github.com/tgermain/grandRepositorySky/node"
+	"github.com/tgermain/grandRepositorySky/shared" // for makeDHTnode
 	"net/http"
-	"time"
+	"time" // to set a timer
 )
+
+//TODO remove this function and replace it by a working one in the lib
+func MakeDHTNode(NewId *string, NewIp, NewPort string) *node.DHTnode {
+	if NewId == nil {
+		tempId := dht.GenerateNodeId()
+		NewId = &tempId
+	}
+	//only one node created by instance
+	shared.LocalId = *NewId
+	shared.LocalIp = NewIp
+	shared.LocalPort = NewPort
+
+	newNode, _ := node.MakeNode()
+	//newNode, commChannel := node.MakeNode()
+	//newComLink := communicator.MakeComlink(newNode, commChannel)
+	//newComLink.StartAndListen()
+
+	return newNode
+}
+
+//one node example
+var id1 string = "01"
+var node1 *node.DHTnode = MakeDHTNode(&id1, "localhost", "1111")
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HelloHandler)
 	r.HandleFunc("/nodes", NodesHandler)
+	r.HandleFunc("/node/{idNoeud}", NodeHandler)
 	http.Handle("/", r)
 
 	go http.ListenAndServe(":3000", r)
@@ -23,14 +51,22 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Hello World too")
 }
 
-func NodesHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "here I will write nodes")
-}
-
 //TODO graph vizualisation => get all nodes ?
 //TODO client all nodes loop request
+func NodesHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, node1.ToString())
+}
 
 //TODO printInfo (fingerTable of a node) request => getNode ?
+func NodeHandler(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	idNoeud := vars["idNoeud"]
+	if idNoeud == "01" {
+		fmt.Fprintf(w, node1.ToString())
+	} else {
+		fmt.Fprintf(w, "noeud non trouve")
+	}
+}
 
 //TODO new node request
 
