3cf1fa84349161864efbc8b63e9dc189431e3be8
tgermain <timothee.germain@wanadoo.fr>
2014-10-09 01:07:17 +0200

    adding a launch parameters

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package grandRepositorySky
 
 import (
+	"github.com/spf13/cobra"
 	"github.com/tgermain/grandRepositorySky/communicator"
 	"github.com/tgermain/grandRepositorySky/dht"
 	"github.com/tgermain/grandRepositorySky/node"
@@ -24,4 +25,23 @@ func MakeDHTNode(NewId *string, NewIp, NewPort string) *node.DHTnode {
 	newComLink.StartAndListen()
 
 	return newNode
+}
+
+func main() {
+	shared.SetupLogger()
+
+	var Id string
+	var Ip string
+	var Port string
+	var DistIp string
+	var DistPort string
+	var join bool
+
+	rootCmd.Flags().StringVarP(&Id, "Id of the node", "n", "2222", "Id you want for your node")
+	rootCmd.Flags().StringVarP(&Ip, "Ip of the node", "i", "localhost", "Ip you want for your node")
+	rootCmd.Flags().StringVarP(&Port, "Port of the node", "p", "2222", "port you want for your node")
+	rootCmd.Flags().BoolVarP(&join, "joining ?", "j", false, "you wanna join ?")
+	rootCmd.Flags().StringVarP(&DistIp, "Ip of the distante node", "w", "localhost", "Ip you want for your node")
+	rootCmd.Flags().StringVarP(&DistPort, "Port of the distante node", "d", "4321", "port you want for your node")
+	rootCmd.Execute()
 }
