9d327ee60ca5f1ea8436cb9cf2d2d907e8bfc899
Aline Martin <aline.martin@insa-lyon.fr>
2014-10-14 03:10:57 +0200

    the web page access now to the fingertable
    the web page access to the power !

--- web/server.go
+++ web/server.go
@@ -4,7 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/tgermain/grandRepositorySky/dht" // for makeDHTnode
+	"github.com/tgermain/grandRepositorySky/communicator/receiver" //for makeDHTnode
+	"github.com/tgermain/grandRepositorySky/dht"                   // for makeDHTnode
 	"github.com/tgermain/grandRepositorySky/node"
 	"github.com/tgermain/grandRepositorySky/shared" // for makeDHTnode
 	"net/http"
@@ -23,35 +24,40 @@ type DistantNodeJSON struct {
 	Port string `json:"port"`
 }
 type NodeJson struct {
-	Id         string `json:"id"`
-	Ip         string `json:"ip"`
-	Port       string `json:"port"`
-	Succesor   string `json:"succesor"`
-	Predecesor string `json:"predecesor"`
+	Id          string          `json:"id"`
+	Ip          string          `json:"ip"`
+	Port        string          `json:"port"`
+	Successor   DistantNodeJSON `json:"successor"`
+	Predecessor DistantNodeJSON `json:"predecessor"`
+	Fingers     []FingerJSON    `json:"fingers"`
 }
 
 //TODO remove this function and replace it by a working one in the lib
 func MakeDHTNode(NewId *string, NewIp, NewPort string) *node.DHTnode {
-	if NewId == nil {
+	if NewId == nil || *NewId == "" {
+
 		tempId := dht.GenerateNodeId()
 		NewId = &tempId
 	}
-	//only one node created by instance
+	shared.Logger.Info("Creating node : \nId %.10v \nIP %s Port %.10s \n", *NewId, NewIp, NewPort)
+	//Set the globally shared information
 	shared.LocalId = *NewId
 	shared.LocalIp = NewIp
 	shared.LocalPort = NewPort
 
-	newNode, _ := node.MakeNode()
-	//newNode, commChannel := node.MakeNode()
-	//newComLink := communicator.MakeComlink(newNode, commChannel)
-	//newComLink.StartAndListen()
+	// create node with its commInterface
+	newNode, newSenderLink := node.MakeNode()
+
+	receiverLink := receiver.MakeReceiver(newNode, newSenderLink)
+	//Make the commInterface listen to incomming messages on globalIp, globalPort
+	receiverLink.StartAndListen()
 
 	return newNode
 }
 
 //one node example
 var id1 string = "01"
-var node1 *node.DHTnode = MakeDHTNode(&id1, "localhost", "1111")
+var node1 *node.DHTnode
 
 // Hello Handler
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
@@ -63,7 +69,31 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 //TODO client all nodes loop request
 func NodesHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("GET /noeuds")
-	node1Json := NodeJson{shared.LocalId, shared.LocalIp, shared.LocalPort, node1.Predecessor.Id, node1.Successor.Id}
+	//gives the local node infos and fingertable
+	fingers := node1.GetFingerTable()
+	fingersJSON := make([]FingerJSON, len(fingers))
+	for key, value := range fingers {
+		if value != nil {
+			nodeResp := value.NodeResp
+			var nodeRespJSON DistantNodeJSON
+			if nodeResp != nil {
+				nodeRespJSON = DistantNodeJSON{nodeResp.Id, nodeResp.Ip, nodeResp.Port}
+			} else {
+				nodeRespJSON = DistantNodeJSON{}
+			}
+			entry := FingerJSON{value.IdKey, nodeRespJSON}
+			fingersJSON[key] = entry
+		} else {
+			entry := FingerJSON{}
+			fingersJSON[key] = entry
+		}
+	}
+	succ := node1.GetSuccesor()
+	succJSON := DistantNodeJSON{succ.Id, succ.Ip, succ.Port}
+	pred := node1.GetPredecessor()
+	predJSON := DistantNodeJSON{pred.Id, pred.Ip, pred.Port}
+
+	node1Json := NodeJson{shared.LocalId, shared.LocalIp, shared.LocalPort, succJSON, predJSON, fingersJSON}
 	js, err := json.Marshal(node1Json)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -100,30 +130,40 @@ func NodeHandler(w http.ResponseWriter, req *http.Request) {
 //TODO launch areYouAlive request
 
 func main() {
+	shared.SetupLogger()
+
 	args := os.Args
-	var port string
-	var ip string
+	port := "3000"
+	ip := "127.0.0.1"
+	portDist := "5000"
+	ipDist := "127.0.0.1"
+	//args : ipDist portDist ip port
 	if len(args) > 1 {
 		if len(args) > 2 {
-			fmt.Printf("%s\n", args[1])
-			port = args[2]
-			fmt.Printf("%s\n", args[2])
-			ip = args[1]
+			if len(args) > 4 {
+				// ipDist portDist ip port
+				port = args[2]
+				ip = args[1]
+				portDist = args[4]
+				ipDist = args[3]
+			} else {
+				// ipDist portDist def_ip def_port
+				port = args[2]
+				ip = args[1]
+			}
 		} else {
-			fmt.Printf("%s\n", args[1])
+			// def_ipDist portDist def_ip def_port
 			port = args[1]
-			// default ip : all
-			ip = ""
 		}
-
-	} else {
-		// default port
-		port = "3000"
 	}
 
+	send := ipDist + ":" + portDist
 	receive := ip + ":" + port
-	fmt.Printf("%s\n", receive)
+	fmt.Printf("dist node : %s\n", send)
+	fmt.Printf("server listen on : %s\n", receive)
 
+	node1 = MakeDHTNode(&id1, ip, port)
+	//TODO add node1 to ring with parameter given
 	r := mux.NewRouter()
 	r.HandleFunc("/", HelloHandler)
 	r.HandleFunc("/nodes", NodesHandler)
