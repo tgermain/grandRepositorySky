8572f6cca7c673332bd9b37dc522a36e826dc9a4
Aline Martin <aline.martin@insa-lyon.fr>
2014-10-16 01:53:58 +0200

    functionnal post request web and server side

--- web/server.go
+++ web/server.go
@@ -28,20 +28,26 @@ type MyServer struct {
 	r *mux.Router
 }
 
+type DataReq struct {
+	Key   string
+	Value string
+}
+
 var node1 *node.DHTnode
+var maxSizePost int64
 
 //Method parts ----------------------------------------------------------
 
 // Hello Handler
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	shared.Logger.Info("GET /")
+	shared.Logger.Notice("GET /")
 	fmt.Fprintf(w, "Hello World too")
 }
 
 //TODO refactor handler url (not really a get nodes)
 //TODO send datas informations too
 func NodesHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("GET /noeuds")
+	shared.Logger.Notice("GET /noeuds")
 
 	node1Json := NodeJson{shared.LocalId, shared.LocalIp, shared.LocalPort, node1.GetSuccesor(), node1.GetPredecessor(), node1.GetFingerTable(), shared.Datas}
 	js, err := json.Marshal(node1Json)
@@ -54,22 +60,32 @@ func NodesHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func DataPostHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("POST new data")
+	shared.Logger.Notice("POST new data")
+
+	decoder := json.NewDecoder(req.Body)
+	var t DataReq
+	err := decoder.Decode(&t)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	node1.SetData(t.Key, t.Value)
 	fmt.Fprintf(w, "ok")
 }
 
 func DataPutHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("PUT data")
+	shared.Logger.Notice("PUT data")
 	fmt.Fprintf(w, "ok")
 }
 
 func DataDeleteHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("DELETE data")
+	shared.Logger.Notice("DELETE data")
 	fmt.Fprintf(w, "ok")
 }
 
 func DataGetHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("GET data")
+	shared.Logger.Notice("GET data")
 	fmt.Fprintf(w, "ok")
 }
 
@@ -109,7 +125,8 @@ func (s *MyServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 func MakeServer(ip string, port string, nod *node.DHTnode) {
 	receive := ip + ":" + port
 	node1 = nod
-	fmt.Printf("server listen on : %s\n", receive)
+	shared.Logger.Notice("server listen on : %s\n", receive)
+	shared.Logger.Warning("datas : %s\n", shared.Datas)
 
 	r := mux.NewRouter()
 	// r.HandleFunc("/", HelloHandler)
@@ -118,10 +135,10 @@ func MakeServer(ip string, port string, nod *node.DHTnode) {
 	r.Handle("/", fs)
 
 	r.HandleFunc("/nodes", NodesHandler)
-	r.HandleFunc("CHORDNODE/storage", DataPostHandler).Methods("POST")
-	r.HandleFunc("CHORDNODE/storage/{key]", DataPutHandler).Methods("PUT")
-	r.HandleFunc("CHORDNODE/storage/{key}", DataDeleteHandler).Methods("DELETE")
-	r.HandleFunc("CHORDNODE/storage/{key}", DataGetHandler).Methods("GET")
+	r.HandleFunc("/storage", DataPostHandler).Methods("POST")
+	r.HandleFunc("/storage/{key]", DataPutHandler).Methods("PUT")
+	r.HandleFunc("/storage/{key}", DataDeleteHandler).Methods("DELETE")
+	r.HandleFunc("/storage/{key}", DataGetHandler).Methods("GET")
 	http.Handle("/", &MyServer{r})
 
 	http.ListenAndServe(receive, nil)
