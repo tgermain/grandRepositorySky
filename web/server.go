3ae28fbce7d736c5549e2e319d7be024f521c8e6
tgermain <timothee.germain@wanadoo.fr>
2014-10-15 19:57:06 +0200

    add charset header

--- web/server.go
+++ web/server.go
@@ -93,6 +93,7 @@ func (s *MyServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	//if origin := req.Header.Get("Origin"); origin != "" {
 	rw.Header().Set("Content-Type", "application/json")
 	rw.Header().Set("Accept", "application/json")
+	rw.Header().Set("Accept-Charset", "utf-8")
 	rw.Header().Set("Access-Control-Allow-Credentials", "true")
 	rw.Header().Set("Access-Control-Allow-Origin", "*")
 	rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
